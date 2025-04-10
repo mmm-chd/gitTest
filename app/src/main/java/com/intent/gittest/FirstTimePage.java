494718e130ce312f74274cabcbae8ac8e71fc68f
Severus Melvin <melvinmulyono02@gmail.com>
2025-04-10 11:54:01 +0700

    feat [SM] first page

---
+++ app/src/main/java/com/intent/gittest/FirstTimePage.java
@@ -0,0 +1,20 @@
+package com.intent.gittest;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class FirstTimePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_first_time_page);
+
+    }
+}
