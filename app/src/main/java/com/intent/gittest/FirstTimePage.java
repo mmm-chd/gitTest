bc350f0389a84f22e12dbc95adcc500a2cead9ba
Severus Melvin <melvinmulyono02@gmail.com>
2025-04-17 09:34:41 +0700

    fix [SM] firstTime-page

--- app/src/main/java/com/intent/gittest/FirstTimePage.java
+++ app/src/main/java/com/intent/gittest/FirstTimePage.java
@@ -1,6 +1,9 @@
 package com.intent.gittest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class FirstTimePage extends AppCompatActivity {
 
+    Button btnSignUp, btnSignIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_first_time_page);
 
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstTimePage.this, RegisterPage.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstTimePage.this, LoginPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
